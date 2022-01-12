5eeebea7bb9d5a34e59f0ad8c6114e698cbaedfe
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-11 20:15:10 -0800

    Buttons

---
+++ Buttons.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class MainWidget(Widget):
+    pass
+
+class Buttons_lab(App):
+    pass
+
+Buttons_lab().run()
--- Buttons_labels.py
+++
@@ -1,2 +0,0 @@
-#buttons
-#test git push
